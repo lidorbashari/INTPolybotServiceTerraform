5553ff3fa189748abf825f9e56a180895c306445
lidor-bashari <lidor1965@gmail.com>
2025-06-01 22:47:00 +0300

    terraform workflow

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,8 +1,3 @@
-output "ami_id" {
-  description = "ami id of chosen region"
-  value       = data.aws_ami.ubuntu.id
-}
-
 output "control_plane_public_ip" {
   description = "public ip of the machines"
   value = module.control_plane_module.public_ip
