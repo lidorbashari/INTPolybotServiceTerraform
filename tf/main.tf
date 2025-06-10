f4c8e6d4c31e79f300884f7c35e639f0f813f98d
lidor-bashari <lidor1965@gmail.com>
2025-06-10 15:15:17 +0300

    terraform workflow

--- tf/main.tf
+++ tf/main.tf
@@ -81,5 +81,7 @@ resource "aws_secretsmanager_secret" "telegram_bot" {
 
 resource "aws_secretsmanager_secret_version" "telegram_bot_version" {
   secret_id     = aws_secretsmanager_secret.telegram_bot.id
-  secret_string = var.telegram_token
+  secret_string = jsonencode({
+    TELEGRAM_BOT_TOKEN = var.telegram_token
+  })
 }
